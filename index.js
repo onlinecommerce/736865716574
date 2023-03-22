9975109570e3d03730ebd39c2b7241b98991b8de
ultron <mulerfrommarss@gmail.com>
2023-03-22 09:58:08 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.use("/home", (req, res, next) => {
+	res.status(200).json({
+		status: "success",
+		message: "This works fine"
+	})
+});
+
+const PORT = 4000;
+app.listen(PORT, () => console.log(`running on port: ${PORT}`));
