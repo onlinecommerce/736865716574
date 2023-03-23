1208e230c6cfb4936ecc04b02a06e00192998801
ultron <mulerfrommarss@gmail.com>
2023-03-23 08:50:02 +0300

    MongoDb user change

--- config/database.js
+++ config/database.js
@@ -1,25 +1,5 @@
-/* const mongoose = require('mongoose');
-
 const connectDataBase = () => {
-    mongoose.connect(process.env.DB_LOCAL_URI).then(con => {
-        console.log(`MongoDB connected with HOST: ${con.connection.host}`);
-    })
-}
-
-module.exports = connectDataBase */
-
-
-// MongoClient.connect(mongoDbUri, settings, function(err, dbref) {
-// if (!err) {
-//   console.log("Mongodb connected");
-//   db = dbref;
-// }else{
-//   console.log("Error while connecting to mongoDB" + err);
-// }
-// });
-
-const connectDataBase = () => {
-    const MONGO_URL = "mongodb+srv://mooler_z:e2dGSpqZVkIFi1Ie@onlineshopping.ulu8wue.mongodb.net/?retryWrites=true&w=majority";
+    const MONGO_URL = "mongodb+srv://mulerfrommarss:12I9FtwXhg4X4YbE@onlineshopping.ulu8wue.mongodb.net/?retryWrites=true&w=majority";
     const mongoose = require('mongoose')
 
     mongoose.Promise = global.Promise;
@@ -34,24 +14,5 @@ const connectDataBase = () => {
         }
     );
 }
-
-/* const connectDataBase = () => {
-    const {
-        MongoClient,
-        ServerApiVersion
-    } = require('mongodb');
-    const uri = "mongodb+srv://mooler_z:e2dGSpqZVkIFi1Ie@onlineshopping.ulu8wue.mongodb.net/?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverApi: ServerApiVersion.v1
-    });
-    client.connect(err => {
-        const collection = client.db("test").collection("devices");
-        console.log(collection);
-        // perform actions on the collection object
-        client.close();
-    });
-} */
 
 module.exports = connectDataBase
