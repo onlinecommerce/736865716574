9e7df17358157ce29086d62cafc918b221785bf9
ultron <mulerfrommarss@gmail.com>
2023-03-27 14:19:02 +0300

    keep mongo alive

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const cors = require('cors')
 
 app.use(cors())
 
-
 app.use(express.json());
 
 // authentication
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 require('./config/database')()
 
 const dotenv = require('dotenv')
+const item = require('./models/item-m.js');
+
+(function() {
+  // to keep mongodb alive because I have encountered that
+  // it stop the service if you didn't interact with it for 24hrs
+  // so it will interact with it in 6hr interval
+  setInterval(async () => {
+    let res = await item.find({id: '1'})
+  }, 1000 * 60 * 60 * 6);
+})() 
 
 dotenv.config({ path: './config/config.env' })
 
