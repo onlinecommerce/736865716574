56718e8ec5dee06000b7953c5e38fdfac8b097fd
ultron <mulerfrommarss@gmail.com>
2023-04-13 14:30:12 +0300

    change database uri

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const connectDataBase = () => {
-  // const MONGO_URL = "mongodb+srv://mulerfrommarss:12I9FtwXhg4X4YbE@onlineshopping.ulu8wue.mongodb.net/?retryWrites=true&w=majority";
-  const MONGO_URL = "mongodb://localhost:27017/foodPrice";
+  const MONGO_URL = "mongodb+srv://mulerfrommarss:12I9FtwXhg4X4YbE@onlineshopping.ulu8wue.mongodb.net/?retryWrites=true&w=majority";
+  // const MONGO_URL = "mongodb://localhost:27017/foodPrice";
   const mongoose = require('mongoose')
 
   mongoose.Promise = global.Promise;
